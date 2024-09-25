84e065bde256b743fd44ac1a38f27b67bc625721
FsmDev <fsmeow@outlook.com>
2024-09-25 01:25:14 -0600

    Moved inventory re-assignment to BarrelBlockEntity constructor

--- src/main/java/net/fsmdev/biggerbarrels/mixin/BarrelBlockEntityMixin.java
+++ src/main/java/net/fsmdev/biggerbarrels/mixin/BarrelBlockEntityMixin.java
@@ -1,19 +1,25 @@
 package net.fsmdev.biggerbarrels.mixin;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BarrelBlockEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BarrelBlockEntity.class)
 public abstract class BarrelBlockEntityMixin {
-    @Shadow
-    private DefaultedList<ItemStack> inventory = DefaultedList.ofSize(this.size(), ItemStack.EMPTY);
+    @Accessor
+    abstract void setInventory(DefaultedList<ItemStack> inventory);
 
     /**
      * @author FsmDev
@@ -22,6 +28,11 @@ public abstract class BarrelBlockEntityMixin {
     @Overwrite
     public int size() {
         return 54;
+    }
+
+    @Inject(method="<init>", at=@At("RETURN"))
+    private void init(BlockPos pos, BlockState state, CallbackInfo ci) {
+        setInventory(DefaultedList.ofSize(this.size(), ItemStack.EMPTY));
     }
 
     /**
